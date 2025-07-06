9a7512078d22b4c38a943e5588865622f50407ab
Kavin Prabakaran K <133646551+OrchidKat@users.noreply.github.com>
2025-07-06 19:50:29 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, render_template
 from app.routes import routes
 from app.models import get_all_events
@@ -15,4 +16,5 @@ def fetch_events():
     return jsonify(events)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets this
+    app.run(host="0.0.0.0", port=port, debug=True)
